057ec160ee67dc1aa45b9f0b3227761ed4f951d6
AlessandroMenconi <alessandro.menconi.00@outlook.it>
2023-04-09 13:59:02 +0200

    fix-email

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'alessandro.menconi.00@outlook.it';
+  $receiving_email_address = 'a.menconi00@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,12 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com.',
+    'username' => 'a.menconi00@gmail.com',
+    'password' => 'Al3M3nc02OO01052000$$',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
